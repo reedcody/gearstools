dc11ba7987066b88fb1e43307cb4ac8f5dec22c7
aparrar1 <35413006+aparrar1@users.noreply.github.com>
2018-10-08 22:47:08 -0700

    Update ImageCollection.extract.R
    Changes in line 87

--- pkg/GEARStools/R/ImageCollection.extract.R
+++ pkg/GEARStools/R/ImageCollection.extract.R
@@ -84,7 +84,7 @@ ImageCollection.extract <- function(ImageCollection,
     if (is.character(extract.sp)) {
       if (file.exists(extract.sp)) {
         extract.sp_read <- st_read(dsn = dirname(extract.sp),
-                                   layer = gsub(c("\\.shp$", "\\.kml$", "\\.kmz$"),"",
+                                   layer = gsub("\\.shp$|\\.kml$|\\.kmz$","",
                                                 basename(extract.sp)))
       }
     } else{
